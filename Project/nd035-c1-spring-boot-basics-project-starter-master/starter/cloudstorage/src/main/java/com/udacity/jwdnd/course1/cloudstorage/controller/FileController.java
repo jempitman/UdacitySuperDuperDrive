246393cfb305b45b0094e68ece2cb785906de68a
jempitman <80392341+jempitman@users.noreply.github.com>
2021-08-09 14:49:27 +0200

    Feat: View uploaded files

--- Project/nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
+++ Project/nd035-c1-spring-boot-basics-project-starter-master/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/FileController.java
@@ -1,8 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import com.udacity.jwdnd.course1.cloudstorage.dto.FileDTO;
+import com.udacity.jwdnd.course1.cloudstorage.model.FileData;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class FileController {
@@ -64,6 +70,7 @@ public class FileController {
         return "result";
     }
 
+    //Delete file from database based on fileId
     @GetMapping("/home/file/delete/{fileId}")
     public String deleteFile(@PathVariable Integer fileId, @ModelAttribute("fileDTO")MultipartFile file, Model model){
 
@@ -73,6 +80,22 @@ public class FileController {
         model.addAttribute("files", fileService.getAllFiles(userService.getLoggedInUsersId()));
         model.addAttribute("result", "fileDelete");
         return "result";
+    }
+
+    //View file
+    @GetMapping("/home/file/view/{fileId}")
+    public StreamingResponseBody viewFile(HttpServletResponse response, @PathVariable Integer fileId) throws IOException{
+        FileData file = fileService.getFileFromFileId(fileId);
+        response.setContentType(file.getContentType());
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,"attachment:filename=\"" + file.getFileName());
+        return outputStream -> {
+            int bytesRead;
+            byte[] buffer = new byte[10000];
+            InputStream inputStream = new ByteArrayInputStream(file.getFileData());
+            while((bytesRead = inputStream.read(buffer)) != -1){
+                outputStream.write(buffer, 0,bytesRead);
+            }
+        };
     }
 
 
