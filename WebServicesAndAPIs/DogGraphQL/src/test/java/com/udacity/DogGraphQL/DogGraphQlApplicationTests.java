2e4825edf36dc08e68531f01a0a0939396c1d5b6
jempitman <80392341+jempitman@users.noreply.github.com>
2021-08-23 15:59:09 +0200

    Feat: query and mutation GraphQL requests

--- WebServicesAndAPIs/DogGraphQL/src/test/java/com/udacity/DogGraphQL/DogGraphQlApplicationTests.java
+++ WebServicesAndAPIs/DogGraphQL/src/test/java/com/udacity/DogGraphQL/DogGraphQlApplicationTests.java
@@ -1,12 +1,11 @@
 package com.udacity.DogGraphQL;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DogGraphQlApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
