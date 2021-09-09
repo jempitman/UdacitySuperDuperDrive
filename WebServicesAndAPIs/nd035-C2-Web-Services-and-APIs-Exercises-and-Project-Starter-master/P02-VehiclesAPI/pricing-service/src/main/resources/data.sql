b12f85fb51ea05946195f3ffeadd2c9eead1384c
jempitman <80392341+jempitman@users.noreply.github.com>
2021-09-09 09:54:47 +0200

    Eureka client registration

---
+++ WebServicesAndAPIs/nd035-C2-Web-Services-and-APIs-Exercises-and-Project-Starter-master/P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('ZAR', 10000.25, 1);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('ZAR', 20000.25, 2);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('ZAR', 30000.25, 3);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('ZAR', 40000.25, 4);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('ZAR', 50000.25, 5);
